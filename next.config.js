f4a3349641607d955feb261a0825e62b7afef1b8
N1kos9 <onkl3p@gmail.com>
2024-01-05 18:00:01 +0000

    solution one

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  experimental: {
+    unstable_runtimeJS: false,
+  },
 };
 
-module.exports = nextConfig;
+module.exports = nextCofig;
