096e190537000a0a626002f54b986cfd77b2e0aa
Nikos <139173199+N1kos9@users.noreply.github.com>
2024-10-16 17:52:15 +0300

    Update header.tsx

--- component/header.tsx
+++ component/header.tsx
@@ -111,7 +111,7 @@ function Header() {
                   </li>
                   <li>
                     <a
-                      href="https://www.linkedin.com/in/murariu-nikos-631366284/"
+                      href="https://www.linkedin.com/in/murariu-nikos-63136628"
                       style={{
                         bottom: navOpen ? "0" : "-20px",
                         opacity: navOpen ? "1" : "0",
